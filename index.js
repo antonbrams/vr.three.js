d1aac7bde5e7ac0ebab0140a8af1f85ec5f9de85
antonkluev <dev@antonkluev.com>
2017-08-03 23:27:28 +0200

    hello world

---
+++ index.js
@@ -0,0 +1,4 @@
+
+export {default as DeviceOrientation} from './DeviceOrientationControls'
+export {default as OrbitControls} 	  from './OrbitControls'
+export {default as StereoEffect} 	  from './StereoEffect'
